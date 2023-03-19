eb1c4c53a4251bf4fc33a81bf6ce68995d5110b4
Shen Nan <wongshennan@gmail.com>
2023-03-19 09:03:01 +0800

    shift to .config

--- configs/zshrc/npm.sh
+++
@@ -1,9 +0,0 @@
-
-# fnm
-export PATH=/Users/wongshen.nan/.fnm:$PATH
-eval "$(fnm env --use-on-cd)"
-
-# pnpm
-export PNPM_HOME="/Users/wongshen.nan/Library/pnpm"
-export PATH="$PNPM_HOME:$PATH"
-# pnpm end
